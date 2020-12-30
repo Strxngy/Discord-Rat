390c48df9b7b3452d9e4502da8ffd2bffec8e40d
Stringy <74079223+Strxngy@users.noreply.github.com>
2020-12-30 20:41:44 +0100

    Update obfusc.py

--- obfusc.py
+++ obfusc.py
@@ -5,6 +5,7 @@ def show(b):
     return b64decode(b).decode()
 
 while True:
+    # add junk code...
     hidden = b'' #base664 encode main.py
     eval(compile(show(hidden), '<string>', 'exec'))
     exit(0)
